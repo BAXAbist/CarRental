893d9fe98d753f0b0f6332f315c79980e51acd73
Alexander Oleynik <62380471+Nurdblav@users.noreply.github.com>
2020-07-21 23:56:01 +0300

    Update AddCar.php

--- Server/AddCar.php
+++ Server/AddCar.php
@@ -3,7 +3,7 @@
     include_once '../db_php/db2.php';
 
     if ($_POST['brand'] && $_POST['cost'] && $_POST['type'] && $_POST['icon'] && $_POST['status'] 
-        && $_POST['passengers'] && $_POST['bags'] && $_POST['doors'] && $_POST['features']) {
+        && $_POST['passengers'] && $_POST['bags'] && $_POST['doors']) {
         $brand = $_POST['brand'];
         $cost = $_POST['cost'];
         $type = $_POST['type'];
@@ -12,11 +12,11 @@
         $passengers = $_POST['passengers'];
         $bags = $_POST['bags'];
         $doors = $_POST['doors'];
-        $features = $_POST['features'];
+        
 
         $db = new DB_requests();
         
-        $data = $db->addCar($brand, $cost, $type, $icon, $status, $passengers, $bags, $doors, $features);
+        $data = $db->addCar($brand, $cost, $type, $icon, $status, $passengers, $bags, $doors);
         if ($data == 'ok') {
             sendResponse(200, $data);
         } else {
