a6be2f6bf25cc016f2199f2384464f83b2e09299
Alexander Oleynik <62380471+Nurdblav@users.noreply.github.com>
2020-07-21 23:52:27 +0300

    Update AddCar.php

--- Server/AddCar.php
+++ Server/AddCar.php
@@ -2,16 +2,21 @@
     include_once 'utils.php';
     include_once '../db_php/db2.php';
 
-    if ($_POST['brand'] && $_POST['cost'] && $_POST['type'] && $_POST['icon'] && $_POST['status']) {
+    if ($_POST['brand'] && $_POST['cost'] && $_POST['type'] && $_POST['icon'] && $_POST['status'] 
+        && $_POST['passengers'] && $_POST['bags'] && $_POST['doors'] && $_POST['features']) {
         $brand = $_POST['brand'];
         $cost = $_POST['cost'];
         $type = $_POST['type'];
         $icon = $_POST['icon'];
         $status = $_POST['status'];
+        $passengers = $_POST['passengers'];
+        $bags = $_POST['bags'];
+        $doors = $_POST['doors'];
+        $features = $_POST['features'];
 
         $db = new DB_requests();
         
-        $data = $db->addCar($brand, $cost, $type, $icon, $status);
+        $data = $db->addCar($brand, $cost, $type, $icon, $status, $passengers, $bags, $doors, $features);
         if ($data == 'ok') {
             sendResponse(200, $data);
         } else {
