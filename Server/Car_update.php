f4c249bea96efdadb3619e3eaa43f01248c66f4b
Alexander Oleynik <62380471+Nurdblav@users.noreply.github.com>
2020-07-21 23:52:56 +0300

    Update Car_update.php

--- Server/Car_update.php
+++ Server/Car_update.php
@@ -8,14 +8,19 @@
         $cost = $_POST['cost'];
         $type = $_POST['type'];
         $icon = $_POST['icon'];
-        $status = $_POST['status'];        
+        $status = $_POST['status'];
+        $passengers = $_POST['passengers'];
+        $bags = $_POST['bags'];
+        $doors = $_POST['doors'];
+        $features = $_POST['features'];        
         
-        if ($brand == null && $cost == null && $type == null && $icon == null && $status == null){
+        if ($brand == null && $cost == null && $type == null && $icon == null && $status == null && $passengers == null
+            && $bags == null && $doors == null && $features == null){
             $data == 'ok';
         }
         else{
             $db = new DB_requests();
-            $data = $db->updateCarInfoById($id_car,$brand,$cost,$type,$icon,$status);
+            $data = $db->updateCarInfoById($id_car,$brand,$cost,$type,$icon,$status, $passengers, $bags, $doors, $features);
         }
         if ($data == 'ok') {
             sendResponse(200, $data);
